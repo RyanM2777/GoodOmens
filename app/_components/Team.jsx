53d70c17c39a7da2171bea39aa3c1853af8a68c5
Ryan Mathison <ryanmathison.com@gmail.com>
2024-05-22 08:53:06 +0100

    Update Team.jsx

--- app/_components/Team.jsx
+++ app/_components/Team.jsx
@@ -22,35 +22,35 @@ const people = [
         name: 'Iain',
         role: '@tattoosbyhank1981',
         imageUrl:
-          '#',
+          'https://media.discordapp.net/attachments/1194400893781688472/1242746504687648829/1598ca4c-0674-4696-90a1-1a4cc8f5dd48.jpg?ex=664ef55f&is=664da3df&hm=27a6c5c44dab976f0cfe462decdbd410773ca9ec4161f123f579b30f1268fbce&=&format=webp&width=526&height=701',
       },
   ]
   
   export default function Example() {
     return (
       <div className="bg-white py-24 sm:py-32">
-        <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-          <div className="max-w-2xl">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet the Experts</h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-            Each expert brings their own artistic flair and attention to detail, ensuring that every piece is not only beautiful but also safely and expertly crafted.
-            </p>
-          </div>
-          <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
-            {people.map((person) => (
-              <li key={person.name}>
-                <div className="flex items-center gap-x-6">
-                  <img className="h-16 w-16 rounded-full" src={person.imageUrl} alt="" />
-                  <div>
-                    <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
-                    <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
-                  </div>
+            <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+                <div className="max-w-2xl">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet the Experts</h2>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                        Each expert brings their own artistic flair and attention to detail, ensuring that every piece is not only beautiful but also safely and expertly crafted.
+                    </p>
                 </div>
-              </li>
-            ))}
-          </ul>
+                <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+                    {people.map((person) => (
+                        <li key={person.name}>
+                            <div className="flex items-center gap-x-6">
+                                <img className="h-16 w-16 rounded-full object-cover" src={person.imageUrl} alt="" />
+                                <div>
+                                    <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
+                                    <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
-      </div>
     )
   }
   
