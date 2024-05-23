44ffa1167294d2fe8ee8b26de48645d096440f2e
Ryan Mathison <ryanmathison.com@gmail.com>
2024-05-23 18:23:52 +0100

    Update Services.jsx

--- app/_components/Services.jsx
+++ app/_components/Services.jsx
@@ -52,7 +52,7 @@ function Services() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5 text-tealGreen-500">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
               </svg>
-              <span className="text-gray-700"> Gentle, You Won't Feel It </span>
+              <span className="text-gray-700"> Gentle, You Won&rsquo;t Feel It </span>
             </li>
           </ul>
           <a href="/services/piercing" className="mt-8 block rounded-full border border-tealGreen-500 bg-tealGreen-500 px-12 py-3 text-center text-sm font-medium text-white hover:bg-tealGreen-600 hover:ring-1 hover:ring-tealGreen-600 focus:outline-none focus:ring active:text-tealGreen-400">
