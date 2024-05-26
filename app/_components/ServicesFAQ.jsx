e82f741fee507054e6534518c58558921a94a68d
Ryan Mathison <ryanmathison.com@gmail.com>
2024-05-26 12:35:56 +0100

    Update ServicesFAQ.jsx

--- app/_components/ServicesFAQ.jsx
+++ app/_components/ServicesFAQ.jsx
@@ -1,86 +1,99 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
 
 function ServicesFAQ() {
+  const [showTattoosFAQ, setShowTattoosFAQ] = useState(false);
+  const [showPiercingsFAQ, setShowPiercingsFAQ] = useState(false);
+
+  const toggleTattoosFAQ = () => {
+    setShowTattoosFAQ(!showTattoosFAQ);
+  };
+
+  const togglePiercingsFAQ = () => {
+    setShowPiercingsFAQ(!showPiercingsFAQ);
+  };
+
   return (
     <div className="bg-tealGreen-100">
-  <div className="container mx-auto px-4 py-6">
-    <h1 className="text-center text-tealGreen-500 text-2xl mb-6">Frequently Asked Questions</h1>
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-      {/* First FAQ Section */}
-      <div className="bg-tealGreen-100">
-      <div className="flex items-center gap-4 bg-tealGreen-100 mb-4">
-          <div className="flex-1 h-px bg-gray-300"></div>
-          <span className="text-gray-700">TATTOOS FAQs</span>
-          <div className="flex-1 h-px bg-gray-300"></div>
-        </div>
-        <div className="mx-auto max-w-lg bg-tealGreen-100">
-          <div className="space-y-6">
-            {/* FAQ Items for the First Section */}
-            <details class="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
-      <summary class="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
-        Can I bring someone to my appointment?
-        <div class="text-secondary-500">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </div>
-      </summary>
-      <div class="px-6 pb-6 text-black">You may bring someone with you to your appointment, however they may not be permitted into the tattoo area. This is for several reasons: <br /> <br /> Due to health and safety- during the procedure we are dealing with bodily fluids and essentially open woulds- we need to minimise the risk of contamination. The dignity of our customers- for some tattoos our customers are required to adjust their clothing and discretion is required. Busy studio- if the studio is busy there may simply not be room for your friend to come along! <br /> <br /> Some exceptions may be permitted, let us know before coming to your appointment. We politely remind customers that our studio is not child friendly, no children are permitted in the main studio and we ask that they are not in the reception area for any extended period of time. We are not babysitters and take no responsibility for children left unattended.</div>
-    </details>
-    <details class="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
-      <summary class="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
-        When can I see my design?
-        <div class="text-black">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </div>
-      </summary>
-      <div class="px-6 pb-6 text-black">Please except your design 24-48hrs before your appointment- this is standard practice for most artists and studios. Without an appointment and a deposit, the artist is essentially working for hours for free. They must create multiple designs for every day they work- these are usually done in their free time, in order and a day or two prior to the appointment (this saves them wasting time over rescheduled or canceled appointments) We have also in the past had customers take the design and tattooed elsewhere- stealing that persons work.</div>
-    </details>
-    <details class="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
-      <summary class="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
-        How should I prep for my Tattoo?
-        <div class="text-black">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </div>
-      </summary>
-      <div class="px-6 pb-6 text-black">Please do not shave or wax the area for 4 days prior to the procedure. We will shave the area on site immediately prior to the tattoo. This is to lessen the likelihood of razor rash, ingrown hairs and cuts- if these were present on the tattoo area we may not be able to tattoo you. Please also do not apply fake tan close to your appointment or soon after - this can have a huge effect on the colour of your finished tattoo and lead to it healing very differently. </div>
-    </details>
-    <details class="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
-      <summary class="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
-      I think I might be pregnant, can I get a tattoo?
-        <div class="text-black">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </div>
-      </summary>
-      <div class="px-6 pb-6 text-black">Short answer - no. Although the risk of infection is incredibly small, it is a possibility if aftercare is not followed. Also, pregnancy can send the body into a healing overdrive and cause the ink to reject, and we dont want that to happen!! </div>
-    </details>
-    <details class="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
-      <summary class="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
-      Is the studio accessible?
-        <div class="text-black">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </div>
-      </summary>
-      <div class="px-6 pb-6 text-black">Yes! We are on one level with disabled access bathrooms. There is a step up into the reception area, please let us know beforehand and we can make sure this is safely navigable for wheelchair users prior to your appointment. </div>
-    </details>
-    <details class="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
-      <summary class="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
-      Payment
-        <div class="text-black">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </div>
-      </summary>
-      <div class="px-6 pb-6 text-black">We are currently a CASH ONLY studio. Artists can be paid by bank transfer or cash only. There are several cash machines less than 50m from the shop entrance.
+      <div className="container mx-auto px-4 py-6">
+        <h1 className="text-center text-tealGreen-500 text-2xl mb-6">Frequently Asked Questions</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {/* First FAQ Section */}
+          <div className="bg-tealGreen-100">
+            <div className="flex items-center gap-4 bg-tealGreen-100 mb-4 cursor-pointer" onClick={toggleTattoosFAQ}>
+              <div className="flex-1 h-px bg-gray-300"></div>
+              <span className="text-gray-700">TATTOOS FAQs</span>
+              <div className="flex-1 h-px bg-gray-300"></div>
+            </div>
+            {showTattoosFAQ && (
+              <div className="mx-auto max-w-lg bg-tealGreen-100">
+                <div className="space-y-6">
+                  {/* FAQ Items for the First Section */}
+                  <details className="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
+                    <summary className="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
+                      Can I bring someone to my appointment?
+                      <div className="text-secondary-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                      </div>
+                    </summary>
+                    <div className="px-6 pb-6 text-black">You may bring someone with you to your appointment, however they may not be permitted into the tattoo area. This is for several reasons: <br /> <br /> Due to health and safety- during the procedure we are dealing with bodily fluids and essentially open woulds- we need to minimise the risk of contamination. The dignity of our customers- for some tattoos our customers are required to adjust their clothing and discretion is required. Busy studio- if the studio is busy there may simply not be room for your friend to come along! <br /> <br /> Some exceptions may be permitted, let us know before coming to your appointment. We politely remind customers that our studio is not child friendly, no children are permitted in the main studio and we ask that they are not in the reception area for any extended period of time. We are not babysitters and take no responsibility for children left unattended.</div>
+                  </details>
+                  <details className="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
+                    <summary className="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
+                      When can I see my design?
+                      <div className="text-black">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                      </div>
+                    </summary>
+                    <div className="px-6 pb-6 text-black">Please except your design 24-48hrs before your appointment- this is standard practice for most artists and studios. Without an appointment and a deposit, the artist is essentially working for hours for free. They must create multiple designs for every day they work- these are usually done in their free time, in order and a day or two prior to the appointment (this saves them wasting time over rescheduled or canceled appointments) We have also in the past had customers take the design and tattooed elsewhere- stealing that persons work.</div>
+                  </details>
+                  <details className="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
+                    <summary className="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
+                      How should I prep for my Tattoo?
+                      <div className="text-black">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5" />
+                        </svg>
+                      </div>
+                    </summary>
+                    <div className="px-6 pb-6 text-black">Please do not shave or wax the area for 4 days prior to the procedure. We will shave the area on site immediately prior to the tattoo. This is to lessen the likelihood of razor rash, ingrown hairs and cuts- if these were present on the tattoo area we may not be able to tattoo you. Please also do not apply fake tan close to your appointment or soon after - this can have a huge effect on the colour of your finished tattoo and lead to it healing very differently. </div>
+                  </details>
+                  <details className="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
+                    <summary className="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
+                      I think I might be pregnant, can I get a tattoo?
+                      <div className="text-black">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                      </div>
+                    </summary>
+                    <div className="px-6 pb-6 text-black">Short answer - no. Although the risk of infection is incredibly small, it is a possibility if aftercare is not followed. Also, pregnancy can send the body into a healing overdrive and cause the ink to reject, and we dont want that to happen!! </div>
+                  </details>
+                  <details className="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
+                    <summary className="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
+                      Is the studio accessible?
+                      <div className="text-black">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                      </div>
+                    </summary>
+                    <div className="px-6 pb-6 text-black">Yes! We are on one level with disabled access bathrooms. There is a step up into the reception area, please let us know beforehand and we can make sure this is safely navigable for wheelchair users prior to your appointment. </div>
+                  </details>
+                  <details className="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
+                    <summary className="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
+                      Payment
+                      <div className="text-black">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="block h-5 w-5 transition-all duration-300 group-open:-rotate-90">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                      </div>
+                    </summary>
+                    <div className="px-6 pb-6 text-black">We are currently a CASH ONLY studio. Artists can be paid by bank transfer or cash only. There are several cash machines less than 50m from the shop entrance.
 
 For any other queries please contact us on the booking form <div className="mt-2 flex items-center justify-center gap-x-6">
               
@@ -88,21 +101,23 @@ For any other queries please contact us on the booking form <div className="mt-2
                 Get in touch <span aria-hidden="true">→</span>
               </a>
             </div> </div>
-    </details>
+                  </details>
+                </div>
+              </div>
+            )}
           </div>
-        </div>
-      </div>
-      {/* Second FAQ Section */}
-      <div className="bg-tealGreen-100">
-        <div className="mx-auto max-w-lg bg-tealGreen-100">
-          <div className="space-y-6">
-          <div className="flex items-center gap-4 bg-tealGreen-100 mb-4">
-          <div className="flex-1 h-px bg-gray-300"></div>
-          <span className="text-gray-700">PIERCINGS FAQs</span>
-          <div className="flex-1 h-px bg-gray-300"></div>
-        </div>
-            {/* FAQ Items for the Second Section */}
-            <details class="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
+          {/* Second FAQ Section */}
+          <div className="bg-tealGreen-100">
+            <div className="flex items-center gap-4 bg-tealGreen-100 mb-4 cursor-pointer" onClick={togglePiercingsFAQ}>
+              <div className="flex-1 h-px bg-gray-300"></div>
+              <span className="text-gray-700">PIERCINGS FAQs</span>
+              <div className="flex-1 h-px bg-gray-300"></div>
+            </div>
+            {showPiercingsFAQ && (
+              <div className="mx-auto max-w-lg bg-tealGreen-100">
+                <div className="space-y-6">
+                  {/* FAQ Items for the Second Section */}
+                  <details class="group rounded-xl bg-tealGreen-100 shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]">
       <summary class="flex cursor-pointer list-none items-center justify-between p-6 text-lg font-medium text-black">
       How old do I need to be?
         <div class="text-secondary-500">
@@ -178,13 +193,14 @@ For any further queries, please contact us on the booking form <div className="m
               
               
     </details>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-  )
+  );
 }
 
-export default ServicesFAQ
+export default ServicesFAQ;
