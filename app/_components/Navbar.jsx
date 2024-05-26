0c9ea460f8ca2c3789cafb4c6d93251f78e0512b
Ryan Mathison <ryanmathison.com@gmail.com>
2024-05-26 21:14:03 +0100

    Update Navbar.jsx

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -67,7 +67,7 @@ export default function Example() {
       </nav>
       <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-51" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-tealGreen-100 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Good Omens</span>
