00046684c4edccc879afda60ddc9b16e656bef18
Ryan Mathison <ryanmathison.com@gmail.com>
2024-05-26 21:13:44 +0100

    Update HomeHeader.jsx

--- app/_components/HomeHeader.jsx
+++ app/_components/HomeHeader.jsx
@@ -53,7 +53,7 @@ export default function Example() {
         </nav>
         <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full max-w-sm overflow-y-auto bg-white px-6 py-6 sm:ring-1 sm:ring-gray-900/10">
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full max-w-sm overflow-y-auto bg-tealGreen-100 px-6 py-6 sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Good Omens</span>
