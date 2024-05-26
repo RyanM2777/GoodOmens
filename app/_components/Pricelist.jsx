57d4923085ba2bc6588075b867319c5f0310a589
Ryan Mathison <ryanmathison.com@gmail.com>
2024-05-26 12:36:18 +0100

    Update Pricelist.jsx

--- app/_components/Pricelist.jsx
+++ app/_components/Pricelist.jsx
@@ -1,36 +1,48 @@
-import React from "react";
+"use client"
+import React, { useState } from 'react';
 
-function Pricelist() {
+const Pricelist = () => {
+  const [showPrices, setShowPrices] = useState(false);
+
+  const togglePrices = () => {
+    setShowPrices(!showPrices);
+  };
   return (
     <section className="bg-tealGreen-100">
-    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-      <div className="mx-auto max-w-screen-lg text-center mb-8 lg:mb-12">
-        <p className="mb-5 font-semibold text-tealGreen-500 text-2xl">
-          Piercing Pricelist
-        </p>
-      </div>
-      <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
-        <div className="flex flex-col p-6 mx-auto max-w-md lg:max-w-full text-center text-gray-900 bg-tealGreen-100 rounded-lg border-gray-100 shadow xl:p-8">
-          <h3 className="mb-4 text-2xl font-semibold">Ear Piercings</h3>
-          <ul role="list" className="mb-8 space-y-4 text-left">
-            <li className="flex items-center justify-between space-x-3">
-              <div className="flex items-center space-x-3">
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span>Lobe (single)</span>
-              </div>
-              <span className="text-right">£25</span>
-            </li>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-lg text-center mb-8 lg:mb-12">
+          <p className="mb-5 font-semibold text-tealGreen-500 text-2xl">
+            Pricelist
+          </p>
+          <div className="flex items-center gap-4 bg-tealGreen-100 cursor-pointer" onClick={togglePrices}>
+            <div className="flex-1 h-px bg-gray-300"></div>
+            <span className="text-gray-700">PIERCING PRICES</span>
+            <div className="flex-1 h-px bg-gray-300"></div>
+          </div>
+        </div>
+        {showPrices && (
+          <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+            <div className="flex flex-col p-6 mx-auto max-w-md lg:max-w-full text-center text-gray-900 bg-tealGreen-100 rounded-lg border-gray-100 shadow xl:p-8">
+              <h3 className="mb-4 text-2xl font-semibold">Ear Piercings</h3>
+              <ul role="list" className="mb-8 space-y-4 text-left">
+                <li className="flex items-center justify-between space-x-3">
+                  <div className="flex items-center space-x-3">
+                    <svg
+                      className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>Lobe (single)</span>
+                  </div>
+                  <span className="text-right">£25</span>
+                </li>
               <li class="flex items-center justify-between space-x-3">
                 <div class="flex items-center space-x-3">
                   <svg
@@ -869,9 +881,11 @@ function Pricelist() {
             </ul>
             
           </div>
-        </div>
+          </div>
+        )}
       </div>
     </section>
   );
-}
+};
+
 export default Pricelist;
