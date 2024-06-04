3599a2331042b76e7d2a22b98d906a6bba8b5ee4
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-04 08:02:14 +0100

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -21,7 +21,7 @@ function page() {
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://goodomens.co.uk/about" />
+      <meta property="og:url" content="https://goodomenstattoo.co.uk/about" />
       <meta property="og:title" content={metadata.title} />
       <meta property="og:description" content={metadata.description} />
       <meta property="og:image" content="/GoodOmensCircle.png" />
@@ -37,7 +37,7 @@ function page() {
  <link rel="icon" href="/GoodOmensCircle.png" type="image/x-icon" />
 
       {/* Canonical Tag */}
-      <link rel="canonical" href="https://www.goodomens.co.uk/about" />
+      <link rel="canonical" href="https://www.goodomenstattoo.co.uk/about" />
     </>
   )
 }
