271d6145d3a1cb1075f2e0278277e537bcaff16f
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-04 07:32:13 +0100

    Update Hero.jsx

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="bg-tealGreen-100">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-3xl text-black">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-4xl text-black">
             Good Omens Tattoo and Piercing
           </h1>
           <p className="max-w-2xl mb-6 font-light text-black lg:mb-8 md:text-lg lg:text-xl">
