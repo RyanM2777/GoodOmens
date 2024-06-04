f19e4d81abab10422e25ac0acb4a77ecde2340fb
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-04 07:28:22 +0100

    Update Hero.jsx

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-black">
-            Payments tool for software companies
+            Good Omens Tattoo and Piercing
           </h1>
           <p className="max-w-2xl mb-6 font-light text-black lg:mb-8 md:text-lg lg:text-xl">
             From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.
