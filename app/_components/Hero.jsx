8cf8c570b00f27ee0be797fe31989f318bfb48fd
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-04 07:17:20 +0100

    Update Hero.jsx

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -6,15 +6,15 @@ const Hero = () => {
   return (
     <>
       <Navbar />
-      <div className="relative bg-white pb-[110px] pt-[120px] dark:bg-dark lg:pt-[150px]">
+      <div className="relative bg-tealGreen-100 pb-[110px] pt-[120px]  lg:pt-[150px]">
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4 lg:w-5/12">
               <div className="hero-content">
-                <h1 className="mb-5 text-4xl font-bold !leading-[1.208] text-dark dark:text-white sm:text-[42px] lg:text-[40px] xl:text-5xl">
+                <h1 className="mb-5 text-4xl font-bold !leading-[1.208] text-dark  sm:text-[42px] lg:text-[40px] xl:text-5xl">
                   Good Omens Tattoo and Piercing
                 </h1>
-                <p className="mb-8 max-w-[480px] text-base text-body-color dark:text-dark-6">
+                <p className="mb-8 max-w-[480px] text-base text-body-color ">
                   Welcome to Good Omens, Dundee&rsquo;s Premier Tattoo & Piercing Studio! Experience a vibrant and welcoming atmosphere at our top-rated studio in Dundee. Our talented artists and piercers are dedicated to transforming your visions into stunning works of art. Whether you&rsquo;re seeking a bold new tattoo, a delicate piece of body art, or a stylish piercing, we are here to provide exceptional quality and unparalleled creativity. Join us to express your individuality in a friendly and professional environment where your satisfaction and comfort are our top priorities.
                 </p>
                 <ul className="flex flex-wrap items-center">
@@ -65,7 +65,7 @@ const Hero = () => {
                   </li>
                 </ul>
                 <div className="clients pt-16">
-                  <h6 className="mb-6 flex items-center text-xs font-normal text-body-color dark:text-dark-6">
+                  <h6 className="mb-6 flex items-center text-xs font-normal text-body-color ">
                     Some Of Our Clients
                     <span className="ml-3 inline-block h-px w-8 bg-body-color"></span>
                   </h6>
@@ -185,38 +185,34 @@ const Navbar = () => {
                   open && "navbarTogglerActive"
                 } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
               >
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color "></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color "></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color"></span>
               </button>
               <nav
                 id="navbarCollapse"
-                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:bg-transparent lg:shadow-none lg:dark:bg-transparent ${
+                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow  lg:static lg:block lg:w-full lg:max-w-full lg:bg-transparent lg:shadow-none lg:dark:bg-transparent ${
                   !open && "hidden"
                 } `}
               >
                 <ul className="block lg:flex">
-                  <ListItem NavLink="/#">Home</ListItem>
-                  <ListItem NavLink="/#">Payment</ListItem>
-                  <ListItem NavLink="/#">About</ListItem>
-                  <ListItem NavLink="/#">Blog</ListItem>
+                  <ListItem NavLink="/">Home</ListItem>
+                  <ListItem NavLink="/about">About Us</ListItem>
+                  <ListItem NavLink="/artists">Artists</ListItem>
+                  <ListItem NavLink="/services">Prices/FAQs</ListItem>
+                  <ListItem NavLink="/events">events</ListItem>
                 </ul>
               </nav>
             </div>
             <div className="hidden justify-end pr-16 sm:flex lg:pr-0">
               <a
-                href="/#"
-                className="px-7 py-3 text-base font-medium text-dark hover:text-primary dark:text-white"
+                href="/contact"
+                className="px-7 py-3 text-base font-medium text-dark hover:text-primary "
               >
-                Sign in
+                Contact
               </a>
 
-              <a
-                href="/#"
-                className="rounded-lg bg-primary px-7 py-3 text-base font-medium text-white hover:bg-opacity-90"
-              >
-                Sign Up
-              </a>
+              
             </div>
           </div>
         </div>
@@ -231,7 +227,7 @@ const ListItem = ({ children, NavLink }) => {
       <li>
         <a
           href={NavLink}
-          className="flex py-2 text-base font-medium text-dark hover:text-primary dark:text-white lg:ml-10 lg:inline-flex"
+          className="flex py-2 text-base font-medium text-dark hover:text-primary  lg:ml-10 lg:inline-flex"
         >
           {children}
         </a>
