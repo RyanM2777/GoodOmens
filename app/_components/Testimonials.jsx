e8a51b4bc157f54b973ef832ed280faf83b8fae5
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-04 07:55:53 +0100

    Update Testimonials.jsx

--- app/_components/Testimonials.jsx
+++ app/_components/Testimonials.jsx
@@ -259,26 +259,25 @@ export default function Example() {
 
   return (
     <section className="relative isolate overflow-hidden bg-tealGreen-200 px-6 py-24 sm:py-32 lg:px-8">
-      <div className="absolute inset-0 -z-10 bg-tealGreen-200 opacity-20" />
-      <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-tealGreen-100 shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
-      <div className="mx-auto max-w-2xl lg:max-w-4xl">
-        <img className="mx-auto h-12" src="/GoodOmens.png" alt="Good Omens Logo" />
-        <figure className="mt-10">
-          <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-            <p>{currentReview.text}</p>
-          </blockquote>
-          <figcaption className="mt-10">
-            
-            <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-              <div className="font-semibold text-gray-900">{currentReview.author}</div>
-              <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
-                <circle cx={1} cy={1} r={1} />
-              </svg>
-              <div className="text-gray-600">{currentReview.source}</div>
-            </div>
-          </figcaption>
-        </figure>
-      </div>
-    </section>
+  <div className="absolute inset-0 -z-10 bg-tealGreen-200" />
+  <div className="absolute inset-0 -z-10" />
+  <div className="mx-auto max-w-2xl lg:max-w-4xl">
+    <img className="mx-auto h-12" src="/GoodOmens.png" alt="Good Omens Logo" />
+    <figure className="mt-10">
+      <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+        <p>{currentReview.text}</p>
+      </blockquote>
+      <figcaption className="mt-10">
+        <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+          <div className="font-semibold text-gray-900">{currentReview.author}</div>
+          <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+            <circle cx={1} cy={1} r={1} />
+          </svg>
+          <div className="text-gray-600">{currentReview.source}</div>
+        </div>
+      </figcaption>
+    </figure>
+  </div>
+</section>
   );
 }
