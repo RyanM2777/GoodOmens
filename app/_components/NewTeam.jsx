bb15da052f2cf38fe93aa8da5e52daa41824eab5
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-08 15:06:51 +0100

    Update NewTeam.jsx

--- app/_components/NewTeam.jsx
+++ app/_components/NewTeam.jsx
@@ -52,7 +52,7 @@ const people = [
 export default function NewTeam() {
     return (
         <div className="bg-tealGreen-200 py-24 sm:py-32">
-            <div className="mx-auto max-w-8xl px-6 lg:px-8">
+            <div className="mx-auto max-w-8xl px-6 lg:px-16">
                 <div className="text-center">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet the Artists</h2>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
