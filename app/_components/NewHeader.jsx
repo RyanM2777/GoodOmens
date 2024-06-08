d2834786b277ab06bdded9a286eadc2a18f0ebab
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-08 13:41:47 +0100

    Update NewHeader.jsx

--- app/_components/NewHeader.jsx
+++ app/_components/NewHeader.jsx
@@ -11,7 +11,7 @@ function NewHeader() {
     backgroundRepeat: "no-repeat",
   }}
 >
-  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+  <div className="mx-auto max-w-screen-2xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
     <div className="mx-auto max-w-2xl text-center">
       <h1 className="text-3xl font-extrabold sm:text-5xl text-white">
         Welcome to
