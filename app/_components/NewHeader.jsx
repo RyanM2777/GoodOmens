dedd7de88d507a00800e9712cb806770fcda88bc
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-08 13:33:21 +0100

    Update NewHeader.jsx

--- app/_components/NewHeader.jsx
+++ app/_components/NewHeader.jsx
@@ -13,17 +13,19 @@ function NewHeader() {
 >
   <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
     <div className="mx-auto max-w-xl text-center">
-      <h1 className="text-3xl font-extrabold sm:text-5x text-white">
+      <h1 className="text-3xl font-extrabold sm:text-5xl text-white">
         Welcome to
         <strong className="font-extrabold text-white sm:block"> Good Omens Tattoo and Piercing </strong>
       </h1>
-
-      <p className="mt-4 sm:text-xl/relaxed">
-        
-      </p>
-
-      
     </div>
+  </div>
+  <div className="mx-auto max-w-screen-xl px-4 py-8 lg:flex lg:items-center lg:justify-center">
+    <p className="sm:text-xl/relaxed text-center bg-black bg-opacity-75 p-4 rounded">
+      Welcome to Good Omens, Dundee&#39;s Premier Tattoo & Piercing Studio!
+      Our talented artists and piercers are dedicated to transforming your visions into stunning works of art. 
+      Whether you&#39;re seeking a bold new tattoo, a delicate piece of body art, or a stylish piercing, we are here 
+      to provide exceptional quality and unparalleled creativity.
+    </p>
   </div>
 </section>
 
