921b395e85689513cee353009a5a82b7174292ba
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-08 13:29:17 +0100

    Update HomeGallery.jsx

--- app/_components/HomeGallery.jsx
+++ app/_components/HomeGallery.jsx
@@ -32,16 +32,16 @@ export function HomeGallery() {
       {selectedImage && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75" onClick={closeModal}>
           <div className="relative">
-            <img src={selectedImage} alt="Enlarged gallery" className="max-w-5xl max-h-2xl rounded-lg" />
+            <img src={selectedImage} alt="Enlarged gallery" className="max-w-3xl max-h-2xl rounded-lg" />
             <button className="absolute top-2 right-2 text-white text-2xl" onClick={closeModal}>&times;</button>
           </div>
         </div>
       )}
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 bg-gray-800">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 bg-tealGreen-500">
         {data.map(({ imageLink }, index) => (
           <div key={index}>
             <img
-              className="h-40 w-full max-w-5xl rounded-lg object-cover object-center cursor-pointer"
+              className="h-40 w-full max-w-3xl rounded-lg object-cover object-center cursor-pointer"
               src={imageLink}
               alt="gallery-photo"
               onClick={() => handleImageClick(imageLink)}
