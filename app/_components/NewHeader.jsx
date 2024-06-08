8ce741e42b38322c9e07170dbc30c93811178d92
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-08 13:10:30 +0100

    Update NewHeader.jsx

--- app/_components/NewHeader.jsx
+++ app/_components/NewHeader.jsx
@@ -2,43 +2,46 @@ import React from 'react'
 
 function NewHeader() {
   return (
-    <section
+   <section
   className="text-black"
   style={{
-    backgroundImage: "url('your-image-url.jpg')",
+    backgroundImage: "url('/goodomens.jpg')",
     backgroundSize: "cover",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
   }}
 >
-  <div className="mx-auto px-4 max-w-screen-xl py-32 lg:flex lg:items-center">
-    <div className="mx-auto mx-15 lg:mx-46 sm:mx-1 text-center">
-      <h1
-        className="bg-gradient-to-r from-tealGreen-500 to-tealGreen-500 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
-      >
-        About Us <br />
-        <span className="sm:block"></span>
+  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+    <div className="mx-auto max-w-xl text-center">
+      <h1 className="text-3xl font-extrabold sm:text-5xl">
+        Understand User Flow.
+        <strong className="font-extrabold text-red-700 sm:block"> Increase Conversion. </strong>
       </h1>
-      <p className="mx-auto mt-4 text-center sm:text-xl/relaxed text-black font-bold">
-        Based in the heart of Dundee City Centre, we pride ourself on being a
-        welcoming and inclusive environment. With nearly 40 years of Tattoo and
-        Piercing experience between us, we can help you get the tattoo or
-        piercing you desire.
-        <br />
-        <br />
-        From small pieces to larger projects, we cover a variety of tattoo
-        styles and themes, with each artist having their own particular favoured
-        style. Please check out the Artists page and look at their work to help
-        you find the artist that is right for you.
-        <div className="mt-2 flex items-center justify-center gap-x-6">
-          <a href="/artists" className="text-sm font-bold leading-6 text-tealGreen-400">
-            Check out our artists <span aria-hidden="true">→</span>
-          </a>
-        </div>
+
+      <p className="mt-4 sm:text-xl/relaxed">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+        numquam ea!
       </p>
+
+      <div className="mt-8 flex flex-wrap justify-center gap-4">
+        <a
+          className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+          href="#"
+        >
+          Get Started
+        </a>
+
+        <a
+          className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+          href="#"
+        >
+          Learn More
+        </a>
+      </div>
     </div>
   </div>
 </section>
+
 
   )
 }
