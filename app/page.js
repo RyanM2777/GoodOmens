3c767058f0279f9d974e7a479b54803718d7f0bb
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-08 13:10:49 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Navbar from "./_components/Navbar";
-import HomeHeader from "./_components/HomeHeader"
+import HomeHeader from "./_components/NewHeader"
 import Team from "./_components/Team"
 import Testimonials from "./_components/Testimonials"
 import Footer from './_components/Footer'
