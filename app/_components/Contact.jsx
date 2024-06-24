45228e22984719ccc26c295970cedfd8aff125ea
Ryan Mathison <ryanmathison.com@gmail.com>
2024-06-24 15:34:31 +0100

    Update Contact.jsx

--- app/_components/Contact.jsx
+++ app/_components/Contact.jsx
@@ -24,177 +24,188 @@ export default function Example() {
           }}
         />
       </div>
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Get in Touch</h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
+      <div className="mx-auto max-w-7xl">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">Get in Touch</h2>
+        <p className="mt-2 text-lg leading-8 text-gray-600 text-center">
           Get in touch regarding: booking, questions & complaints
         </p>
-      </div>
-      <form action="https://formsubmit.co/Hello@goodomenstattoo.co.uk" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
-        
-        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-            
-          <div>
-            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
-              First name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autoComplete="family-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="Phone Number" className="block text-sm font-semibold leading-6 text-gray-900">
-              Phone Number
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="number"
-                name="phone number"
-                id="phone nuber"
-                autoComplete="phone number"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
-              Are you over the age of 18
-            </label>
-            <div className="relative mt-2.5">
-              <div className="absolute inset-y-0 left-0 flex items-center">
-                
-                <select
-                  id="over 18"
-                  name="over 18"
-                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-900 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                >
-                  <option>Yes</option>
-                  <option>No</option>
-                </select>
-                <ChevronDownIcon
-                  className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                  aria-hidden="true"
-                />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 mt-16">
+          <form action="https://formsubmit.co/Hello@goodomenstattoo.co.uk" method="POST" className="space-y-6">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
               </div>
-              <input
-                type="number"
-                name="over 18"
-                id="over 18"
-                autoComplete=""
-                className="block w-1 rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+              <div>
+                <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                  Last name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="Phone Number" className="block text-sm font-semibold leading-6 text-gray-900">
+                  Phone Number
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="number"
+                    name="phone number"
+                    id="phone nuber"
+                    autoComplete="phone number"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+                  Are you over the age of 18
+                </label>
+                <div className="relative mt-2.5">
+                  <div className="absolute inset-y-0 left-0 flex items-center">
+                    <select
+                      id="over 18"
+                      name="over 18"
+                      className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-900 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                    >
+                      <option>Yes</option>
+                      <option>No</option>
+                    </select>
+                    <ChevronDownIcon
+                      className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <input
+                    type="number"
+                    name="over 18"
+                    id="over 18"
+                    autoComplete=""
+                    className="block w-1 rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="artist" className="block text-sm font-semibold leading-6 text-gray-900">
+                  Do you have a specific artist in mind?
+                </label>
+                <div className="mt-2.5">
+                  <select
+                    id="artist"
+                    name="artist"
+                    required
+                    className="block w-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  >
+                    <option value="" disabled selected>Select an option</option>
+                    <option className="text-white" value="Lauren">Lauren</option>
+                    <option className="text-white" value="Iain">Iain</option>
+                    <option className="text-white" value="Steven">Steven</option>
+                    <option className="text-white" value="Emily">Emily</option>
+                    <option className="text-white" value="No">No</option>
+                  </select>
+                  <ChevronDownIcon
+                    className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+                  Message
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows={4}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    defaultValue={''}
+                  />
+                </div>
+              </div>
+              <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+                <div className="flex h-6 items-center">
+                  <Switch
+                    checked={agreed}
+                    onChange={setAgreed}
+                    className={classNames(
+                      agreed ? 'bg-tealGreen-400' : 'bg-gray-200',
+                      'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                    )}
+                  >
+                    <span className="sr-only">Agree to policies</span>
+                    <span
+                      aria-hidden="true"
+                      className={classNames(
+                        agreed ? 'translate-x-3.5' : 'translate-x-0',
+                        'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
+                      )}
+                    />
+                  </Switch>
+                </div>
+                <Switch.Label className="text-sm leading-6 text-gray-600">
+                  By selecting this, you agree to our{' '}
+                  <a href="#" className="font-semibold text-tealGreen-400">
+                    privacy&nbsp;policy
+                  </a>
+                  .
+                </Switch.Label>
+              </Switch.Group>
             </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="artist" className="block text-sm font-semibold leading-6 text-gray-900">
-              Do you have a specific artist in mind?
-            </label>
-            <div className="mt-2.5">
-              <select
-                id="artist"
-                name="artist"
-                required
-                className="block w-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+            <div className="mt-10">
+              <button
+                type="submit"
+                className="block w-full rounded-md bg-tealGreen-400 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                <option value="" disabled selected>Select an option</option>
-                <option className="text-white" value="Lauren">Lauren</option>
-                <option className="text-white" value="Iain">Iain</option>
-                <option className="text-white" value="Steven">Steven</option>
-                <option className="text-white" value="Emily">Emily</option>
-                <option className="text-white" value="No">No</option>
-              </select>
-              <ChevronDownIcon
-                className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                aria-hidden="true"
-              />
+                Let&#39;s talk
+              </button>
             </div>
+          </form>
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="text-2xl font-semibold text-gray-900 mb-6">Where you can find us</h3>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2204.419483710449!2d-2.971106322775656!3d56.460534037258114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48865d723d71d5ad%3A0x94219622fff272f!2sGood%20Omens%20Tattoo!5e0!3m2!1sen!2suk!4v1719239423719!5m2!1sen!2suk"
+              width="400"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
           </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
-              Message
-            </label>
-            <div className="mt-2.5">
-              <textarea
-                name="message"
-                id="message"
-                rows={4}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={''}
-              />
-            </div>
-          </div>
-          <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-            <div className="flex h-6 items-center">
-              <Switch
-                checked={agreed}
-                onChange={setAgreed}
-                className={classNames(
-                  agreed ? 'bg-tealGreen-400' : 'bg-gray-200',
-                  'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                )}
-              >
-                <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  className={classNames(
-                    agreed ? 'translate-x-3.5' : 'translate-x-0',
-                    'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
-                  )}
-                />
-              </Switch>
-            </div>
-            <Switch.Label className="text-sm leading-6 text-gray-600">
-              By selecting this, you agree to our{' '}
-              <a href="#" className="font-semibold text-tealGreen-400">
-                privacy&nbsp;policy
-              </a>
-              .
-            </Switch.Label>
-          </Switch.Group>
         </div>
-        <div className="mt-10">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-tealGreen-400 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Let&#39;s talk
-          </button>
-        </div>
-      </form>
+      </div>
     </div>
   )
 }
